6d2d4a8cf4fa07ed1717af9c7da1160b7a3b11e7
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-06-18 20:38:47 -0300

    adding a basic form and some functionality

--- src/components/DataEntry.vue
+++ src/components/DataEntry.vue
@@ -1,32 +1,86 @@
 <template>
-    <span>
-        I bought {{this.quantityAcquired}}
-        {{ this.coinList[0].coin }} 
-        with {{ this.investedAmount }} 
-        {{ this.coinList[1].coin }}
-    </span>
+  <form @submit.prevent>
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">I bought </label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="quantity acquired" />
+          </p>
+        </div>
+        <div class="field">
+          <div class="select">
+            <select v-model="selectedVolatile">
+              <option disabled value="">Coin</option>
+              <option
+                v-for="coin in volatileCoins"
+                :key="coin.id"
+                :value="coin.name"
+              >
+                {{ coin.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="field-label is-normal">
+          <label class="label"> with </label>
+        </div>
+        <div class="field">
+          <p class="control is-expanded">
+            <input type="text" class="input" placeholder="invested amount" />
+          </p>
+        </div>
+        <div class="field">
+          <div class="select">
+            <select v-model="selectedStable">
+              <option disabled value="">Coin</option>
+              <option
+                v-for="coin in stableCoins"
+                :key="coin.id"
+                :value="coin.name"
+              >
+                {{ coin.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
-
 export default {
-  name: "DataEntry",
+  name: 'DataEntry',
   data() {
-      return {
-          coinList: [{
-              id: 1,
-              coin: 'DOT',
-              type: 'volatile'
-          },
-          {
-              id: 2,
-              coin: 'BUSD',
-              type: 'stable'
-          }],
-          investedAmount: 15,
-          quantityAcquired: 9.78
-
-      }
-  }
-};
+    return {
+      coinList: [
+        {
+          id: 1,
+          name: 'DOT',
+          type: 'volatile',
+        },
+        {
+          id: 2,
+          name: 'BUSD',
+          type: 'stable',
+        },
+      ],
+      investedAmount: 15,
+      quantityAcquired: 9.78,
+      selectedVolatile: '',
+      selectedStable: '',
+    }
+  },
+  computed: {
+    volatileCoins() {
+      return this.coinList.filter((coin) => coin.type === 'volatile')
+    },
+    stableCoins() {
+      return this.coinList.filter((coin) => coin.type === 'stable')
+    },
+  },
+}
 </script>
