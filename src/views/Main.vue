e82f0dd0188ed0707ad97b29588ed20e3088cce9
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-08-08 01:23:19 -0300

    functionality for language change

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -53,5 +53,40 @@ export default {
       description: ``,
     }
   },
+  methods: {
+    changeLanguage() {
+      if (this.currentLanguage === `ENG`) {
+        this.currentLanguage = `SPA`
+      } else {
+        this.currentLanguage = `ENG`
+      }
+      return this.currentLanguage
+    },
+    changeElementsLanguage() {
+      const currentLanguage = this.changeLanguage()
+      let currentTitle = ``
+      let currentDescription = ``
+
+      if (currentLanguage === `ENG`) {
+        currentTitle = this.getTitle(currentLanguage)
+        currentDescription = this.getDescription(currentLanguage)
+      }
+
+      if (currentLanguage === `SPA`) {
+        currentTitle = this.getTitle(currentLanguage)
+        currentDescription = this.getDescription(currentLanguage)
+      }
+
+      this.title = currentTitle
+      this.description = currentDescription
+    },
+    getTitle(currentLanguage) {
+      const title = this.appTitles.filter((el) => {
+        if (el.language === currentLanguage) {
+          return el
+        }
+      })
+      return title[0].text
+    },
 }
 </script>
