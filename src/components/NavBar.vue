32bc9f0cd3ee3a6204f91462a6b3d3e8e56d920c
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-08-09 18:51:49 -0300

    basic navbar styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,17 @@
 <template>
-  <span> This is a Navigation Bar! </span>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong></strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
