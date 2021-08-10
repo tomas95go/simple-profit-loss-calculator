be440469df6e9e9c165516b5281f15fd0789771a
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-08-10 18:50:03 -0300

    installing bulma

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,6 +8,13 @@
     <div class="navbar-menu">
       <div class="navbar-end">
         <div class="navbar-item">
+          <button class="button is-dark" :class="isDark">
+            <span class="icon is-small">
+              <FontAwesomeIcon :icon="icon" :size="size" :style="style" />
+            </span>
+          </button>
+        </div>
+        <div class="navbar-item">
           <LangBtn
             @changeLanguage="this.$emit('changeLanguage')"
             :currentLanguage="currentLanguage"
@@ -20,15 +27,26 @@
 
 <script>
 import LangBtn from '@/components/LangBtn.vue'
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
 export default {
   name: 'NavBar',
   components: {
     LangBtn,
+    FontAwesomeIcon,
   },
   props: {
     currentLanguage: String,
     theme: String,
+  },
+  data() {
+    return {
+      icon: faSun,
+      size: '2x',
+      style: {
+        color: 'yellow',
+      },
+    }
   },
   computed: {
     isDark() {
